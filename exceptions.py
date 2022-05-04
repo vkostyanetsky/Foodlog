46f19c0d12b15bfaa21e3db46bd1b7d8da6fefce
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-05-04 08:46:30 +0700

    Deprecated code removal (pt. 2)

--- exceptions.py
+++ exceptions.py
@@ -6,12 +6,3 @@ class CatalogEntryNotFound(Exception):
         self.message = "Catalog's entry \"{}\" is not found.".format(entry_title)
 
         super().__init__(self.message)
-
-
-class CatalogNotFound(Exception):
-
-    def __init__(self, path):
-
-        self.message = "Catalog's file \"{}\" is not found.".format(path)
-
-        super().__init__(self.message)

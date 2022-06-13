71cbc2c5a102dd2750cd37c4d4483f6926eae82a
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-06-13 10:51:18 +0700

    Reformatted the code

--- calc.py
+++ calc.py
@@ -7,7 +7,6 @@ class CatalogEntryNotFound(Exception):
     pass
 
     def __init__(self, entry_title):
-
         self.message = "Catalog's entry \"{}\" is not found.".format(entry_title)
 
         super().__init__(self.message)

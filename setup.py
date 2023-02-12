d3be7fc5cd833235f5dcd4ce0a2897f205ea07dc
vkostyanetsky <vlad@kostyanetsky.ru>
2023-02-12 22:40:28 +0400

    Code polishing

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+The package description.
+"""
+
 from setuptools import setup
 from foodlog.version import __version__
 
+with open("README.md", encoding="utf-8-sig") as readme_file:
+    long_description = readme_file.read()
 
 setup(
     name="foodlog",
     version=__version__,
     description="A simple CLI timer to calculate fasting zones.",
-    long_description=open("README.md", encoding="utf-8-sig").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/vkostyanetsky/Foodlog",
     license="MIT",
